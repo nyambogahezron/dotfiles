b14203e33b46ea90af44653f7af074ae26e0b096
nyambogahezron <hezzyn1@gmail.com>
2026-05-13 16:52:21 +0300

    update filw name

--- setup/docker.sh
+++ setup/docker.sh
@@ -55,6 +55,7 @@ install_docker() {
     
     # Install docker-compose
     if ! command_exists docker-compose; then
+        sudo apt install gnome-terminal
         print_step "Installing Docker Compose..."
         sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
